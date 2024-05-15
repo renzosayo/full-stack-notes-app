0ecc346e242477869a299b55076d3694e57f0404
renzosayo* <71843954+renzosayo@users.noreply.github.com>
2024-05-15 17:53:30 +0800

    Complete footer by adding 'center' class

---
+++ src/views/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer center">
+      <p className="footer__text">
+        Created by{" "}
+        <a
+          className="footer__text__link"
+          href="https://www.linkedin.com/in/john-renzo-sayo-227261285/"
+        >
+          John Renzo Sayo
+        </a>
+      </p>
+    </div>
+  );
+}
