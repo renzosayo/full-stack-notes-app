e098a29f9b2aeb26fb9d107dbb20c48c729cf779
renzosayo* <71843954+renzosayo@users.noreply.github.com>
2024-05-16 18:53:49 +0800

    Add CSS class for showing ellipsis on overflow

--- src/views/components/NoteCard.tsx
+++ src/views/components/NoteCard.tsx
@@ -5,9 +5,9 @@ import dateFormat from "dateformat";
 export default function NoteCard({ note }: { note: Note }) {
   return (
     <div className="note-card note-main__card">
-      <h2 className="note-card__title">{note.title}</h2>
-      <p className="note-card__body">{note.body}</p>
-      <p className="note-card__date">
+      <h2 className="note-card__title ellipsis">{note.title}</h2>
+      <p className="note-card__body ellipsis">{note.body}</p>
+      <p className="note-card__date ellipsis">
         Last modified:{" "}
         {dateFormat(note.dateWritten, "dddd, mmmm dS, yyyy, h:MM TT")}
       </p>
