619e470a1def6789e2291b228d6f2bbbfc494fc6
renzosayo* <71843954+renzosayo@users.noreply.github.com>
2024-05-22 11:01:06 +0800

    Add route for deleting notes

--- src/server/routes/notesRouter.ts
+++ src/server/routes/notesRouter.ts
@@ -6,61 +6,59 @@ const router = express.Router();
 
 const dbHandler = createDBHandler();
 
-router.get("/", (req: Request, res: Response) => {
-  try {
-    dbHandler.readAllNotes().then((result) => {
-      return res.json(result);
-    });
-  } catch (e) {
-    console.log(e);
-  }
-});
-
-router.post("/create", (req: Request, res: Response) => {
-  try {
-    const newNote: Note = {
-      _id: null,
-      title: req.body.title,
-      body: req.body.body || null,
-      dateWritten: new Date(),
-    };
-    dbHandler.createNote(newNote).then(() => {
-      console.log(`${newNote.title} added`);
-      res.send(`${newNote.title} added`);
-    });
-  } catch (e) {
-    console.log(e);
-  }
-});
-
-router.post("/update/:id", (req: Request, res: Response) => {
-  try {
-    console.log("update");
-    const id = req.params.id;
-    const note = {
-      title: req.body.title,
-      body: req.body.body || null,
-      dateWritten: new Date(),
-    };
-    dbHandler.updateNote(id, note).then(() => {
-      console.log(`${note.title} updated`);
-      res.send(`${note.title} updated`);
-    });
-  } catch (e) {
-    console.log(e);
-  }
-});
-
-router.post("/delete", (req: Request, res: Response) => {
-  try {
-    const id: string = req.body.id;
-    dbHandler.deleteNote(id).then(() => {
-      console.log(`${id} deleted`);
-      res.send(`${id} deleted`);
-    });
-  } catch (e) {
-    console.log(e);
-  }
-});
+router
+  .get("/", (req: Request, res: Response) => {
+    try {
+      dbHandler.readAllNotes().then((result) => {
+        return res.json(result);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  })
+  .post("/", (req: Request, res: Response) => {
+    try {
+      const newNote: Note = {
+        _id: null,
+        title: req.body.title,
+        body: req.body.body || null,
+        dateWritten: new Date(),
+      };
+      dbHandler.createNote(newNote).then(() => {
+        console.log(`${newNote.title} added`);
+        res.send(`${newNote.title} added`);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  })
+  .put("/", (req: Request, res: Response) => {
+    try {
+      console.log("update");
+      const id = req.body._id;
+      const note = {
+        title: req.body.title,
+        body: req.body.body || null,
+        dateWritten: new Date(),
+      };
+      dbHandler.updateNote(id, note).then(() => {
+        console.log(`${note.title} updated`);
+        res.send(`${note.title} updated`);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  })
+  .delete("/", (req: Request, res: Response) => {
+    try {
+      const id: string = req.body.id;
+      dbHandler.deleteNote(id).then(() => {
+        console.log(`${id} deleted`);
+        res.send(`${id} deleted`);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  });
 
 export default router;
