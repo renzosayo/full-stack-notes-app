9404ff91825c0d527ef59f232e29ce2aa4a147ba
renzosayo* <71843954+renzosayo@users.noreply.github.com>
2024-05-22 09:39:17 +0800

    Refactor api call to separate function

--- src/utils/handleEvent.ts
+++ src/utils/handleEvent.ts
@@ -2,20 +2,34 @@ export function handleSaveNewNote({
   title,
   body,
 }: {
-  title: String;
-  body: String;
+  title: string;
+  body: string;
 }): void {
   const URI = "http://localhost:3000/notes/create";
+  Promise.resolve(callApi(URI, "POST", JSON.stringify({ title, body })));
+}
 
-  (async function () {
-    await fetch(URI, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ title, body }),
-    });
-  })();
-  // Bug: fetch post not pushing note to database
+export function handleClickUpdateNote({
+  _id,
+  title,
+  body,
+}: {
+  _id: string;
+  title: string;
+  body: string;
+}) {
+  const URI = `http://localhost:3000/notes/update/${_id}`;
+  alert(URI);
+  Promise.resolve(callApi(URI, "POST", JSON.stringify({ title, body })));
+}
+
+export async function callApi(URI: string, method?: string, body?: string) {
+  return await fetch(URI, {
+    method,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body,
+  });
 }
